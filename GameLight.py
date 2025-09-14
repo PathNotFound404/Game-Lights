2c40267f5bf4a9490e2e2a3bd0ae672c1b5958bf
PathNotFound404 <c.pinkston2004@gmail.com>
2025-09-14 14:42:01 -0500

    post code added to change light color

--- GameLight.py
+++ GameLight.py
@@ -1,16 +1,38 @@
 from http.client import responses
 import json
 import requests
+import uuid
 
 API_KEY = '4440531d-e039-4dcf-9b60-406486bae545'
-url = 'https://openapi.api.govee.com/router/api/v1/user/devices'
+geturl = 'https://openapi.api.govee.com/router/api/v1/user/devices'
+posturl = 'https://openapi.api.govee.com/router/api/v1/device/control'
+id = str(uuid.uuid4())
+
+color = ((158 & 0xFF) << 16) | ((0 & 0xFF) << 8) | ((255 & 0xFF) << 0)
 
 headers = {
     'Content-Type': 'application/json',
     'Govee-API-Key': API_KEY
 }
 
-response = requests.get(url, headers=headers)
+payload = {
+    "requestId": id,
+    "payload": {
+        "sku": "H6008",
+        "device": "72:83:D4:AD:FC:B5:BB:EE",
+        "capability": {
+            "type": "devices.capabilities.color_setting",
+            "instance": "colorRgb",
+            "value": color
+        }
+    }
+}
+
+##Get call
+##response = requests.get(geturl, headers=headers)
+
+response = requests.post(posturl,headers=headers, json=payload)
+
 
 if response.status_code == 200:
     devices = response.json()
@@ -19,3 +41,5 @@ if response.status_code == 200:
     print(json.dumps(devices, indent=4))
 else:
     print(f"Failed to fetch devices: {response.status_code} - {response.text}")
+
+print(color)
