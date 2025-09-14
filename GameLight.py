ba25a5b8489c8d19a5ed1233ca317d4bd7b4d151
PathNotFound404 <c.pinkston2004@gmail.com>
2025-09-14 18:27:20 -0500

    Made Change Color a function

--- GameLight.py
+++ GameLight.py
@@ -1,39 +1,57 @@
 from http.client import responses
 import json
+from time import sleep
+
 import requests
 import uuid
 
 API_KEY = '4440531d-e039-4dcf-9b60-406486bae545'
 geturl = 'https://openapi.api.govee.com/router/api/v1/user/devices'
 posturl = 'https://openapi.api.govee.com/router/api/v1/device/control'
-id = str(uuid.uuid4())
 
-color = ((158 & 0xFF) << 16) | ((0 & 0xFF) << 8) | ((255 & 0xFF) << 0)
 
-headers = {
-    'Content-Type': 'application/json',
-    'Govee-API-Key': API_KEY
-}
 
-payload = {
-    "requestId": id,
-    "payload": {
-        "sku": "H6008",
-        "device": "72:83:D4:AD:FC:B5:BB:EE",
-        "capability": {
-            "type": "devices.capabilities.color_setting",
-            "instance": "colorRgb",
-            "value": color
+
+def change_color(r, g, b,):
+    color = ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | ((b & 0xFF) << 0)
+    request_id = str(uuid.uuid4())
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Govee-API-Key': API_KEY
+    }
+
+    payload = {
+        "requestId": request_id,
+        "payload": {
+            "sku": "H6008",
+            "device": "72:83:D4:AD:FC:B5:BB:EE",
+            "capability": {
+                "type": "devices.capabilities.color_setting",
+                "instance": "colorRgb",
+                "value": color
+            }
         }
     }
-}
+    response = requests.post(posturl,headers=headers, json=payload)
+
+
+
+change_color(255,0,0)
+##sleep(.1)
+change_color(0,255,0)
+##sleep(.1)
+change_color(0,0,255)
+print("done")
+
 
 ##Get call
 ##response = requests.get(geturl, headers=headers)
 
-response = requests.post(posturl,headers=headers, json=payload)
 
 
+
+"""
 if response.status_code == 200:
     devices = response.json()
     print("Devices:", devices)
@@ -41,5 +59,4 @@ if response.status_code == 200:
     print(json.dumps(devices, indent=4))
 else:
     print(f"Failed to fetch devices: {response.status_code} - {response.text}")
-
-print(color)
+"""
