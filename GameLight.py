f159902e43e48c1a9a7f4164f326ac8c8cb76646
Cody Pinkston <c.pinkston2004@gmail.com>
2025-09-24 16:55:42 -0500

    Game lights now works but is very laggy

--- GameLight.py
+++ GameLight.py
@@ -4,6 +4,8 @@ from time import sleep
 import requests
 import uuid
 import config
+from PIL import ImageGrab
+import numpy as np
 
 #url to post requst
 posturl = 'https://openapi.api.govee.com/router/api/v1/device/control'
@@ -14,8 +16,8 @@ posturl = 'https://openapi.api.govee.com/router/api/v1/device/control'
 
 
 #Function to change the color of each light in the Device List
-def change_color(r, g, b,):
-    color = ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | ((b & 0xFF) << 0)
+def change_color(color):
+
 
     for d in config.devices:
         request_id = str(uuid.uuid4())
@@ -44,15 +46,53 @@ def change_color(r, g, b,):
             print(f"Failed to fetch devices: {response.status_code} - {response.text}")
 
 
-change_color(255,0,0)
-##sleep(.1)
-change_color(0,255,0)
-##sleep(.1)
-change_color(0,0,255)
+def make_color_int(r, g, b) -> int:
+        rgb = ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | ((b & 0xFF) << 0)
+        return rgb
+
+def get_avg_color() -> int:
+    ##Get Screenshot then make array
+    img = ImageGrab.grab()
+    ##img_np = np.array(img)
+
+    img_arr = np.frombuffer(img.tobytes(), dtype=np.uint8)
+    img_arr = img_arr.reshape((img.size[1], img.size[0], 3))
+
+    ##Get avg color of image
+    step = 25
+    num_pixels = 1080//step * 1920//step
+    r = g = b = 0
+
+    for y in range(0, 1080, step):
+        for x in range(0, 1920, step):
+            px = img_arr[y][x]
+            r += px[0]
+            g += px[1]
+            b += px[2]
+    r = r//num_pixels
+    g = g//num_pixels
+    b = b//num_pixels
+
+    color = make_color_int(r,g,b)
+
+    img.close()
+    return color
+
+
+
+
+##change_color(255,0,0)
+##sleep(1)
+##change_color(0,255,0)
+##sleep(1)
+##change_color(0,0,255)
+
+x = 0
+while(True):
+    change_color(int(get_avg_color()))
+    print(get_avg_color())
+    x = x + 1
+    ##sleep(1)
+
+
 print("done")
-
-
-
-
-
-
