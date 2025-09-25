08defa19b9c763e40258b0d7d9db3148c6c1e045
Cody Pinkston <c.pinkston2004@gmail.com>
2025-09-24 19:05:27 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 #This is where you will enter the api-key you have recived from Govee
-api_key = '4440531d-e039-4dcf-9b60-406486bae545'
+api_key = 'YOUR KEY HERE'
 
 #Here is where you will put the device number for each light you
 #Would like to modify
 #To get this number make sure your api-key is entered above and run
 #getDevices.py to get a file called "devices.txt" that will list all your devices
-devices = ["00:68:D4:AD:FC:E6:F3:4E","FB:EA:D4:AD:FC:E6:F6:74"]
+devices = ["YOUR","DEVICES","HERE"]
